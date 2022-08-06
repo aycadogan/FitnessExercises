fdbb0f5695a70d8677b94cb7f59698a6019b0357
aycadogan89 <doganayca89@gmail.com>
2022-08-06 02:41:21 -0700

    exercise detail page

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -8,6 +8,29 @@ import ExerciseCard from './ExerciseCard'
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1)
   const exercisesPerPage = 9
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = []
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData(
+          'https://exercisedb.p.rapidapi.com/exercises',
+          exerciseOptions
+        )
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        )
+      }
+
+      setExercises(exercisesData)
+    }
+
+    fetchExercisesData()
+  }, [bodyPart])
+
   // Pagination
   const indexOfLastExercise = currentPage * exercisesPerPage
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
@@ -15,14 +38,14 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     indexOfFirstExercise,
     indexOfLastExercise
   )
-  const paginate = (event, value) => {
+  const paginate = (e, value) => {
     setCurrentPage(value)
 
     window.scrollTo({ top: 1800, behavior: 'smooth' })
   }
 
   return (
-    <Box id='exercises' sx={{ mt: { lg: '109px' } }} mt='50px' p='20px'>
+    <Box id='exercises' sx={{ mt: { lg: '110px' } }} mt='50px' p='20px'>
       <Typography
         variant='h4'
         fontWeight='bold'
@@ -33,7 +56,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       </Typography>
       <Stack
         direction='row'
-        sx={{ gap: { lg: '107px', xs: '50px' } }}
+        sx={{ gap: { lg: '110px', xs: '50px' } }}
         flexWrap='wrap'
         justifyContent='center'
       >
